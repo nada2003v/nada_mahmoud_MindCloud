584b4f27f9340c5b92db04422b4e7d4a77466b17
nada mahmoud <nada2003v@gmail.com>
2023-10-21 09:25:03 +0200

    Adding the favourite team to python file

--- nada.py
+++ nada.py
@@ -2,9 +2,10 @@ name = "Nada Mahmoud"
 age = 20
 collegeDep = "Electrical Power and Machines"
 yearOfGrad = 2026
-
+favTeam = "Hardware"
 
 print("my name is "+ name)
 print("my age is "+ age)
 print("my department is "+ collegeDep)
 print("my year of graduation is "+ yearOfGrad)
+print("my favourite team is "+ favTeam)
