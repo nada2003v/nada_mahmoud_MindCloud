f873d2f7adae93dbeb5c3319083f8c1104cdc246
nada mahmoud <nada2003v@gmail.com>
2023-10-21 09:23:31 +0200

    Adding the year of graduation to python file

--- nada.py
+++ nada.py
@@ -1,6 +1,10 @@
 name = "Nada Mahmoud"
 age = 20
 collegeDep = "Electrical Power and Machines"
+yearOfGrad = 2026
+
+
 print("my name is "+ name)
 print("my age is "+ age)
 print("my department is "+ collegeDep)
+print("my year of graduation is "+ yearOfGrad)
